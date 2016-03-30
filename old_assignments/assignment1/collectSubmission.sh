4d54d822c92d452e4f636e3ac64c0b05734b9616
Gregory King <greg.james.king@gmail.com>
2016-03-30 14:07:46 -0700

    Adding assignment collection script;

---
+++ old_assignments/assignment1/collectSubmission.sh
@@ -0,0 +1,10 @@
+rm -f assignment1.zip 
+zip -r assignment1.zip . -x "*.git*" "*cs224d/datasets*" "*.ipynb_checkpoints*" "*README.md" "*collectSubmission.sh" "*requirements.txt" \
+	"saved_params_1000.npy" "saved_params_2000.npy" "saved_params_3000.npy" "saved_params_4000.npy" "saved_params_5000.npy" \
+	"saved_params_6000.npy" "saved_params_7000.npy" "saved_params_8000.npy" "saved_params_9000.npy" "saved_params_10000.npy" \
+	"saved_params_11000.npy" "saved_params_12000.npy" "saved_params_13000.npy" "saved_params_14000.npy" "saved_params_15000.npy" \
+	"saved_params_16000.npy" "saved_params_17000.npy" "saved_params_18000.npy" "saved_params_19000.npy" "saved_params_20000.npy" \
+	"saved_params_21000.npy" "saved_params_22000.npy" "saved_params_23000.npy" "saved_params_24000.npy" "saved_params_25000.npy" \
+	"saved_params_26000.npy" "saved_params_27000.npy" "saved_params_28000.npy" "saved_params_29000.npy" "saved_params_30000.npy" \
+	"saved_params_31000.npy" "saved_params_32000.npy" "saved_params_33000.npy" "saved_params_34000.npy" "saved_params_35000.npy" \
+	"saved_params_36000.npy" "saved_params_37000.npy" "saved_params_38000.npy" "saved_params_39000.npy"
