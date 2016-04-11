f021da1395bfb5f86442221aca5630c324b3aa51
Gregory King <greg.james.king@gmail.com>
2016-04-11 12:06:08 -0700

    Used 2to3 to update python3 support.

--- assignment1/q3_sgd.py
+++ assignment1/q3_sgd.py
@@ -5,7 +5,7 @@ import glob
 import random
 import numpy as np
 import os.path as op
-import cPickle as pickle
+import pickle as pickle
 
 def load_saved_params():
     """ A helper function that loads previously saved parameters and resets iteration start """
@@ -69,7 +69,7 @@ def sgd(f, x0, step, iterations, postprocessing = None, useSaved = False, PRINT_
     
     expcost = None
     
-    for iter in xrange(start_iter + 1, iterations + 1):
+    for iter in range(start_iter + 1, iterations + 1):
         ### Don't forget to apply the postprocessing after every iteration!
         ### You might want to print the progress every few iterations.
 
@@ -83,7 +83,7 @@ def sgd(f, x0, step, iterations, postprocessing = None, useSaved = False, PRINT_
                 expcost = cost
             else:
                 expcost = .95 * expcost + .05 * cost
-            print "iter %d: %f" % (iter, expcost)
+            print("iter %d: %f" % (iter, expcost))
         
         if iter % SAVE_PARAMS_EVERY == 0 and useSaved:
             save_params(iter, x)
@@ -96,20 +96,20 @@ def sgd(f, x0, step, iterations, postprocessing = None, useSaved = False, PRINT_
 def sanity_check():
     quad = lambda x: (np.sum(x ** 2), x * 2)
 
-    print "Running sanity checks..."
+    print("Running sanity checks...")
     t1 = sgd(quad, 0.5, 0.01, 1000, PRINT_EVERY=100)
-    print "test 1 result:", t1
+    print("test 1 result:", t1)
     assert abs(t1) <= 1e-6
 
     t2 = sgd(quad, 0.0, 0.01, 1000, PRINT_EVERY=100)
-    print "test 2 result:", t2
+    print("test 2 result:", t2)
     assert abs(t2) <= 1e-6
 
     t3 = sgd(quad, -1.5, 0.01, 1000, PRINT_EVERY=100)
-    print "test 3 result:", t3
+    print("test 3 result:", t3)
     assert abs(t3) <= 1e-6
     
-    print ""
+    print("")
 
 def your_sanity_checks(): 
     """
@@ -118,7 +118,7 @@ def your_sanity_checks():
     This function will not be called by the autograder, nor will
     your additional tests be graded.
     """
-    print "Running your sanity checks..."
+    print("Running your sanity checks...")
     ### YOUR CODE HERE
     raise NotImplementedError
     ### END YOUR CODE
