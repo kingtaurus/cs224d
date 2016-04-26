92ebad32604309019eb22fcdbbd7c839c3797993
Gregory King <greg.james.king@gmail.com>
2016-04-26 15:15:23 -0700

    Implemented glorot normalization;

--- old_assignments/assignment2/misc.py
+++ old_assignments/assignment2/misc.py
@@ -6,7 +6,8 @@ from numpy import *
 
 def random_weight_matrix(m, n):
     #### YOUR CODE HERE ####
-
+    eps = sqrt(6/(m+n))
+    A0 = random.uniform(low=-eps, high=eps, size=(m,n))
     #### END YOUR CODE ####
     assert A0.shape == (m,n)
     return A0
