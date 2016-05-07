b6b982951959050bf6a695e26fd27b212da4bae8
Gregory King <greg.james.king@gmail.com>
2016-05-07 01:48:52 -0700

    Python2 to python3 conversions

--- assignment2/q2_initialization.py
+++ assignment2/q2_initialization.py
@@ -34,7 +34,7 @@ def test_initialization_basic():
   """
   Some simple tests for the initialization.
   """
-  print "Running basic tests..."
+  print( "Running basic tests...")
   xavier_initializer = xavier_weight_init()
   shape = (1,)
   xavier_mat = xavier_initializer(shape)
@@ -43,7 +43,7 @@ def test_initialization_basic():
   shape = (1, 2, 3)
   xavier_mat = xavier_initializer(shape)
   assert xavier_mat.get_shape() == shape
-  print "Basic (non-exhaustive) Xavier initialization tests pass\n"
+  print( "Basic (non-exhaustive) Xavier initialization tests pass\n")
 
 def test_initialization():
   """ 
@@ -52,7 +52,7 @@ def test_initialization():
   This function will not be called by the autograder, nor will
   your tests be graded.
   """
-  print "Running your tests..."
+  print( "Running your tests...")
   ### YOUR CODE HERE
   raise NotImplementedError
   ### END YOUR CODE  
