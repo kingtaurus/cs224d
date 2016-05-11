73aabf90e25df908a0819c75630a8dc0fedb3973
Gregory King <greg.james.king@gmail.com>
2016-05-11 15:59:17 -0700

    Commiting code; have to replan solution;

--- assignment1/tests/test_word2vec_to_solutions.py
+++ assignment1/tests/test_word2vec_to_solutions.py
@@ -33,7 +33,6 @@ def rel_error(x,y):
     """ returns relative error """
     return np.max(np.abs(x - y) / (np.maximum(1e-8, np.abs(x) + np.abs(y))))
 
-
 @pytest.fixture(scope='module')
 def dataset_default():
     dataset = type('dummy', (), {})()
@@ -46,21 +45,38 @@ def dataset_default():
            for i in range(2*C)]
     dataset.sampleTokenIdx   = dummySampleTokenIdx
     dataset.getRandomContext = getRandomContext
+    dataset.genTokens        = ["a", "b", "c", "d", "e"]
+    dataset.dummy_tokens     = dict((i,j) for j,i in enumerate(dataset.genTokens()))
+    dataset.dummy_vectors    = normalizeRows(np.random(10,3))
     return dataset
 
 @pytest.fixture(scope='module')
 def dataset_large(size = 10):
-    dataset = type('dummy', (), {})()
     assert size < 26
-    def dummySampleTokenIdx():
-        return random.randint(0, size+1)
+    dataset = type('dummy', (), {})()
 
-    def getRandomContext(C):
-        tokens = [chr(i + ord('a')) for i in range(0, size+1)]
-        return tokens[random.randint(0,size+1)], [tokens[random.randint(0,size+1)] \
+    def dummySampleTokenIdx():
+        return random.randint(0, size)
+
+    def gen_tokens():
+        tokens = [chr(i + ord('a')) for i in range(0, size)]
+        return tokens
+
+    def getRandomContext(C = size):
+        tokens = gen_tokens()
+        return tokens[random.randint(0,size-1)], [tokens[random.randint(0,size-1)] \
            for i in range(2*C)]
+
+    dataset.size             = size
     dataset.sampleTokenIdx   = dummySampleTokenIdx
     dataset.getRandomContext = getRandomContext
+    dataset.genTokens        = gen_tokens()
+    dataset.dummy_tokens     = dict((i,j) for j,i in enumerate(dataset.genTokens))
+    dataset.dummy_vectors    = normalizeRows(np.random.randn(size * 2, 5))
     return dataset
 
 
+def test_skipgram_to_solutions(dataset_large):
+    word2vec_sgd_wrapper(skipgram, dataset_large.dummy_tokens, dataset_large.dummy_vectors, dataset_large, 5)
+    #this might be harder than it looks (since the number of calls to random have to be identical;)
+    #will probably need to re-work this
