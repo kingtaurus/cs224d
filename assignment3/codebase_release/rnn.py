c52f51093d6201bdda6277cd48e8baaa5d250c77
Gregory King <greg.james.king@gmail.com>
2016-05-17 13:58:01 -0700

    Added variable summary function;

--- assignment3/codebase_release/rnn.py
+++ assignment3/codebase_release/rnn.py
@@ -12,6 +12,17 @@ from utils import Vocab
 
 from collections import OrderedDict
 
+def variable_summaries(variable, name):
+  with tf.name_scope("summaries"):
+    mean = tf.reduce_mean(variable)
+    tf.scalar_summary('mean/' + name, mean)
+    with tf.name_scope('stddev'):
+      stddev = tf.sqrt(tf.reduce_sum(tf.square(variable - mean)))
+    tf.scalar_summary('stddev/' + name, stddev)
+    tf.scalar_summary('max/' + name, tf.reduce_max(variable))
+    tf.scalar_summary('min/' + name, tf.reduce_min(variable))
+    tf.histogram_summary(name, variable)
+
 RESET_AFTER = 50
 class Config(object):
     """Holds model hyperparams and data information.
